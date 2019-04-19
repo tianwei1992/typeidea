e3afaceaf68f2b8b0830214342d5add814a1fefd
grace <tianweigrace@qq.com>
2019-04-19 17:33:46 +0800

    Fix: Model中带choices的IntegerField不适合用forms.BooleanField展示

--- typeidea/blog/adminforms.py
+++ typeidea/blog/adminforms.py
@@ -9,5 +9,5 @@ class PostAdminForm(forms.ModelForm):
         class PostAdmin(admin.ModelAdmin):
             form = PostAdminForm
     """
-    status = forms.BooleanField(label="是否删除", required=True)  # TODO: 处理布尔类型为我们需要的字段
+    # status = forms.BooleanField(label="是否删除", required=True)  # status原本是个choice，这里修改成Boolean形式不合适……
     desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
