e762435db6c67c85701625e6d6c52e563b718619
grace <tianweigrace@qq.com>
2019-05-03 21:04:28 +0800

    划分”主题“，例如default，simple还有bootstrap，templates和staticfiles一起构成主题

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'typeidea.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'themes','default','templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+THEME = 'bootstrap'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static', 'themes', THEME, 'static'),
 ]
